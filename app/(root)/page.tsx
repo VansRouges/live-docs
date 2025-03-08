0ae04ed6d88eab8b953f56ef99a363a3c678f781
freeguilders <freeguilders@gmail.com>
2025-03-08 01:31:51 +0000

    added jsm editor

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        Hello from home
+        <Button>Click Me</Button>
+      </div>
+    </main>
+  );
+}
+
